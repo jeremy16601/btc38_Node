4d7fce2a94f2ddfe1416bd5e29dafed922712fc7
jeremy16601 <lixueqing1127@gmail.com>
2017-10-10 22:30:49 +0800

    修改算法

--- app/service/robot.js
+++ app/service/robot.js
@@ -5,7 +5,7 @@ module.exports = app => {
 
         //得到最新价格
         * getPrice(coinname) {
-            const result = yield this.ctx.curl('http://localhost:7002/getDepth?coinname=' + coinname, {
+            const result = yield this.ctx.curl('http://localhost:7001/getDepth?coinname=' + coinname, {
                 method: 'GET',
                 dataType: 'json',
             });
@@ -16,39 +16,43 @@ module.exports = app => {
             //当前最新价格
             const nowPrice = bids[0];
             console.log('最新价格INF：' + nowPrice[0]);
-            return nowPrice;
+            return nowPrice[0];
         }
 
         //开启机器人
         * startRobot() {
             //获取价格
-            const nowPrice = yield this.getPrice('INF');
+            let price = yield this.getPrice('INF');
             //开始时间
             let currentTime = Date.parse(new Date());
             //结束时间
-            const endTime = parseInt(currentTime + (60 * 1000));
+            let endTime = parseInt(currentTime + (6* 1000));
             const self = this;
             //开启循环
             var interval = setInterval(
                 function () {
                     co(function* () {
                         currentTime = Date.parse(new Date());
-
                         console.log('执行了循环currentTime=' + currentTime + '   endTime=' + endTime);
                         if (currentTime == endTime) {
-                            //结束时候获取价格和之前的对比
-                            const newData = yield self.getPrice('INF');
-                            
-                            console.log('结束。。。原始价格：' + nowPrice + '最新价格：' + JSON.stringify(newData));
-                            //停止循环
-                            clearInterval(interval);
+                            //结束时候获取价格和之前的对比 
+                            const currentPrice = yield self.getPrice('INF');
+                            //如果原始价格跟当前价格相差2.2%  买入
+                            const mPrice=parseFloat(price)-parseFloat(price*0.022); 
+                            console.warn('结束：原始价格：' + price + '最新价格：' +  currentPrice+'一分钟后2.2% 的价格：'+mPrice.toFixed(5));
+                            if(currentPrice<mPrice  ) {
+                                console.error('买入！')
+                            }                       
+                            // console.log('价格没变化，重新开始。')
+                            price = yield self.getPrice('INF');
+                            endTime = parseInt(currentTime + (6* 1000));
                         };
                     })
                 }, 1000);
 
-            return nowPrice;
+            return price;
         }
-
+    
     }
     return Robot;
 };
